65918a59af690cb18bce7db6576a24a03596aa5f
Ygor <ygor.20260279@institutojef.org.br>
2026-09-24 20:58:57 -0300

    fiz a pagina inicial

---
+++ SERVLET123/src/main/java/com/example/servlet123/Servlet/Servlet.java
@@ -0,0 +1,70 @@
+package com.example.servlet123.Servlet;
+//doGet deve ser usado apenas para buscar e ler informaoces, sem alterar nada no banco de dados
+// (ex: carregar a lista de livros, pesquisar um autor, etc).
+//
+//doPost deve ser usado para enviar e processar acoes
+// (ex: cadastrar um usuario, registrar um empréstimo, fazer login).
+//
+//O comando request carrega os dados que o cliente enviou para o servidor
+// (exemplo: formulário prenchido)
+// O comando response devolve o resultado final ao navegador após a requisição
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(urlPatterns = {"/Servlet", "/paginaInicio"})
+public class Servlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Ygor";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+
+
+
+    public void destroy() {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+//        response.setContentType("text/html");
+//        PrintWriter outFile = response.getWriter();
+//        //coiso inicial do html
+//        outFile.println("<!DOCTYPE html>");
+//        outFile.println("<html lang=\"pt-BR\">\n");
+//        outFile.println("<head>");
+//        outFile.println("<meta charset=\"UTF-8\">\n");
+//        outFile.println("<title>🦣🦣🦣🦣</title>\n");
+//        outFile.println("</head>");
+//
+//        outFile.println("<body>");
+//        outFile.println("SIGMA BOY🫡🫡😶‍🌫️😶‍🌫️😶‍🌫️💣💣🗿🗿😎");
+//
+//        outFile.println("</body>");
+//    }
